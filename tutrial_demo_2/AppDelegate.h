0f29978607bbea755f6a5a4ccc043e7c00153638
math-miki <math.micky.dtl@gmail.com>
2015-12-18 01:22:03 +0900

    Initial Commit

---
+++ tutrial_demo_2/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  tutrial_demo_2
+//
+//  Created by Miki Takahashi on 2015/12/18.
+//  Copyright © 2015年 Miki Takahashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ tutrial_demo_2Tests/tutrial_demo_2Tests.m
@@ -0,0 +1,39 @@
+//
+//  tutrial_demo_2Tests.m
+//  tutrial_demo_2Tests
+//
+//  Created by Miki Takahashi on 2015/12/18.
+//  Copyright © 2015年 Miki Takahashi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tutrial_demo_2Tests : XCTestCase
+
+@end
+
+@implementation tutrial_demo_2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
